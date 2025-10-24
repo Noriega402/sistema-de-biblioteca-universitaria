f7112c0b67f6d1d22a60847e3477cef236e12782
Noriega402 <d.noriegachajon@gmail.com>
2025-10-23 18:05:36 -0600

    Configuracion de permisos para gateway en registro de usuarios

--- auth-service/src/main/java/com/umg/auth_service/security/SecurityConfig.java
+++ auth-service/src/main/java/com/umg/auth_service/security/SecurityConfig.java
@@ -1,31 +1,35 @@
 package com.umg.auth_service.security;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableMethodSecurity
-@RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/register", "/auth/login", "/actuator/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+    SecurityFilterChain security(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable) // APIs JSON: desactiva CSRF
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(reg -> reg
+                .requestMatchers(HttpMethod.POST, "/auth/register", "/auth/login").permitAll()
+                .requestMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
-                );
-        return http.build();
+                )
+                .build();
     }
 
     @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // fuerza por defecto 10
     }
 }
